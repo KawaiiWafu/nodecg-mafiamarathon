42773f93c55f57e5570025f60bee307efe88d07f
KawaiiWafu <remiliass@gmail.com>
2021-07-30 12:02:37 +0200

    Add files via upload

--- extension.js
+++ extension.js
@@ -3,11 +3,25 @@
 module.exports = function (nodecg) {
 	const router = nodecg.Router();
 	const finalTimes = nodecg.Replicant('finalTimes', {defaultValue: {}, persistent: false});
+	const timer = nodecg.Replicant('timer', 'nodecg-speedcontrol');
 
 	router.get('/endRun', (req, res) => {
-		finalTimes.value[req.query.runner] = req.query.time;
-		nodecg.log.info(finalTimes.value);
+		finalTimes.value[req.query.runner] = {"igt": req.query.time, "rta": timer.value.time};
 		res.send('');
+	});
+
+	router.get('/startTimer', (req, res) => {
+		if (req.query.key == nodecg.bundleConfig.key) {
+			nodecg.sendMessageToBundle('timerStart', 'nodecg-speedcontrol');
+			res.send('Timer started');
+		}
+		else {
+			res.send('Invalid key');
+		}
+	});
+
+	router.get('/finalTimes', (req, res) => {
+		res.json(finalTimes.value);
 	});
 
 	nodecg.mount('/nodecg-mafiamarathon', router);
